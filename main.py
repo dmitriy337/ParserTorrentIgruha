ca530dae5f1fe57141fb07d73735aa9da2bd6d6a
ᐯㄩ卄丨乙卂 ᐯㄩ卄 <47759698+dmitriy337@users.noreply.github.com>
2020-09-04 16:13:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+from prettytable import PrettyTable
+from time import sleep
+
+table = PrettyTable()
+table.field_names = ['GameName','GameLink']
+
+def main(PageId):
+    request = requests.get(f'https://torrent-igruha.org/newgames/page/{PageId}/')
+    page = BeautifulSoup(request.content, 'html.parser')
+    MainBlock = page.find('div',class_='articles-film-cuted')
+    GameBlocks = MainBlock.findAll('div',class_='article-film')
+    for GameBlock in GameBlocks:
+        GameName = (GameBlock.find(class_='article-film-title')).text
+        GameLink = ((GameBlock.find(class_='article-film-title')).find('a'))['href']
+        table.add_row([GameName,GameLink])
+    
+for i in range(1,95):
+    main(i)
+open('Table.txt','a').write(str(table))  
